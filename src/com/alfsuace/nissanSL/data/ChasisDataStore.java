8ce78cd685a037f023b675b43b3bddf34be97a09
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-01-30 17:30:00 +0100

    singleton de ChasisDataStore.

--- src/com/alfsuace/nissanSL/data/ChasisDataStore.java
+++ src/com/alfsuace/nissanSL/data/ChasisDataStore.java
@@ -1,4 +1,29 @@
 package com.alfsuace.nissanSL.data;
 
+import com.alfsuace.nissanSL.domain.models.Chassis;
+
+import java.util.List;
+
 public class ChasisDataStore {
+    private static ChasisDataStore instance = null;
+    private TreeMap<Integer, Chassis> dataStore = new TreeMap<>();
+
+    public void add(Chassis chassis){
+        dataStore.put(chassis.getFrameCode(), chassis);
+    }
+    public Chassis get(String frameCode){
+        return dataStore.get(frameCode);
+    }
+
+    public List<Chassis> getAll(){
+        return (List<Chassis>) dataStore.values();
+    }
+    public static ChasisDataStore getInstance(){
+        if(instance==false){
+            instance=new ChasisDataStore();
+        }
+        return instance;
+    }
+
+
 }
